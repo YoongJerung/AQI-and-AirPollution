128780ffde062f2223ca86b25d847537dbee01ca
YoongJerung <82308090+YoongJerung@users.noreply.github.com>
2023-06-19 21:03:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToPosition() {
+    console.log("hello");
+    window.scrollTo({
+        top: 770,
+        behavior: "smooth"
+    });
+}
+
+function toRegion(href) {
+    window.location.href = href;
+}
